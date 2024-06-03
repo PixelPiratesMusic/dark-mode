91e927adba4e72f6c59d188dc926e91b2607e212
Pixel Pirates <tobauntat@gmail.com>
2024-06-03 16:01:59 +0200

    background > backgroundImage

--- script.js
+++ script.js
@@ -12,25 +12,31 @@ window.onload = function () {
 
     function toggle(event) {
         if(event.target.checked){
-            // Go dark mode
-            body.style.background = 'linear-gradient(180deg, #060c15 0%, #172443 100%)';
-            body.style.color = '#ffffff';
-            gradient.src = 'images/dark-bg.png'
-            logo.src = 'images/logo-light.png';
-            headline.innerHTML = 'Dark';
-            textDay.style.color = '#ffffff4d';
-            textNight.style.color = '#ffffff';
-            portrait.style.filter = 'grayscale(0.3) brightness(0.5)';
+            goDark();
         } else {
-            // Go light mode
-            body.style.background = 'linear-gradient(180deg, #cbd3f1 0%, #ffffff 100%)';
-            body.style.color = '#000000';
-            gradient.src = 'images/light-bg.png'
-            logo.src = 'images/logo-dark.png';
-            headline.innerHTML = 'Light';
-            textDay.style.color = '#000000';
-            textNight.style.color = '#0000004d';
-            portrait.style.filter = 'grayscale(0) brightness(1)';
+            goLight();
         }
+    }
+
+    function goDark(){
+        body.style.backgroundImage = 'linear-gradient(180deg, #060c15 0%, #172443 100%)';
+        body.style.color = '#ffffff';
+        gradient.src = 'images/dark-bg.png'
+        logo.src = 'images/logo-light.png';
+        headline.innerHTML = 'Dark';
+        textDay.style.color = '#ffffff4d';
+        textNight.style.color = '#ffffff';
+        portrait.style.filter = 'grayscale(0.3) brightness(0.5)';
+    }
+
+    function goLight() {
+        body.style.backgroundImage = 'linear-gradient(180deg, #cbd3f1 0%, #ffffff 100%)';
+        body.style.color = '#000000';
+        gradient.src = 'images/light-bg.png'
+        logo.src = 'images/logo-dark.png';
+        headline.innerHTML = 'Light';
+        textDay.style.color = '#000000';
+        textNight.style.color = '#0000004d';
+        portrait.style.filter = 'grayscale(0) brightness(1)';
     }
 };
