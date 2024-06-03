a0f11b5274c0b5d2c2c9dbdf9ac580c2d3d456e3
Pixel Pirates <tobauntat@gmail.com>
2024-06-03 16:08:14 +0200

    color correction

--- script.js
+++ script.js
@@ -20,23 +20,23 @@ window.onload = function () {
 
     function goDark(){
         body.style.backgroundImage = 'linear-gradient(180deg, #060c15 0%, #172443 100%)';
-        body.style.color = '#ffffff';
+        body.style.color = '#d3d3d3';
         gradient.src = 'images/dark-bg.png'
         logo.src = 'images/logo-light.png';
         headline.innerHTML = 'Dark';
-        textDay.style.color = '#ffffff4d';
-        textNight.style.color = '#ffffff';
+        textDay.style.color = '#d3d3d34d';
+        textNight.style.color = '#d3d3d3';
         portrait.style.filter = 'grayscale(0.3) brightness(0.5)';
     }
 
     function goLight() {
         body.style.backgroundImage = 'linear-gradient(180deg, #cbd3f1 0%, #ffffff 100%)';
-        body.style.color = '#000000';
+        body.style.color = '#262626';
         gradient.src = 'images/light-bg.png'
         logo.src = 'images/logo-dark.png';
         headline.innerHTML = 'Light';
-        textDay.style.color = '#000000';
-        textNight.style.color = '#0000004d';
+        textDay.style.color = '#262626';
+        textNight.style.color = '#262626';
         portrait.style.filter = 'grayscale(0) brightness(1)';
     }
 };
